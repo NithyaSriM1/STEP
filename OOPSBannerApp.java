e006d03dab566f9e9e1c29d4badd89018fbbbeac
NithyaSriM1 <nithyasrimurugesan1510@gmail.com>
2026-03-03 10:07:45 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,62 @@
+package uc3;
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "
+        ));
 
+        // Line 2
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "
+        ));
+
+        // Line 3
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                "*       *",
+                "*        "
+        ));
+
+        // Line 4
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                " ******  ",
+                "  *****  "
+        ));
+
+        // Line 5
+        System.out.println(String.join(" ",
+                "*       *",
+                "*       *",
+                "*        ",
+                "       * "
+        ));
+
+        // Line 6
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                "*        ",
+                " *     * "
+        ));
+
+        // Line 7
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "*        ",
+                "  *****  "
+        ));
     }
 }
